fe197daffc877c2ae3c38b9a57c1e84213618946
silva-leonardo <silva.leonardo7843@gmail.com>
2024-06-25 12:29:35 -0300

    Primeiro Exercicio do Curso

---
+++ exercicio1_aula8.py
@@ -0,0 +1,27 @@
+#1º Exercicio de Python -> Recriar o codigo a seguir, e criar as respectivas variaveis.
+
+# print('Nome:', nome)
+# print('Sobrenome:', sobrenome)
+# print('Idade:', idade)
+# print('Ano de Nascimento:', ano_nascimento)
+# print('É maior de Idade ?:', maior_de_idade)
+# print('Altura em Metros::', altura_metros)
+
+
+#Resolução do Exercicio
+nome= 'Leonardo'
+sobrenome= 'Da Silva'
+idade= 22
+ano_nascimento= 2001
+maior_de_idade= idade>=18
+altura_metros= 1.73
+
+
+#Exercicio
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Nome Completo:', nome, sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de Idade ?:', maior_de_idade)
+print('Altura em Metros::', altura_metros)
